20e23c1aa6a4a7217c9b8352d35a7ccf266d6c47
belgray <belg186@yandex.ru>
2025-04-27 19:15:16 +0300

    Improve write and scan methods

--- BGConsole/module.py
+++ BGConsole/module.py
@@ -15,27 +15,31 @@ class BGC:
         return random.choice(list(Param))
 
     @staticmethod
-    def write(text: str, param: Param = None, color: Color = None):
-        if param == None:
-            param = Param.NULL
-        if color == None:
-            color = Color.NULL
+    def write(text: str, param: Param = Param.NULL, color: Color = Color.NULL) -> str:
+        """Print the stylized text
+
+        :argument text: Text
+        :argument param: Text parameter
+        :argument color: Text color
+
+        :returns: Stylized text
+        """
 
         print(Tool.OFF.value + str(param.value) + str(color.value) + str(text) + Tool.OFF.value)
         return Tool.OFF.value + str(param.value) + str(color.value) + str(text) + Tool.OFF.value
 
     @staticmethod
-    def scan(label: str, label_param: Param = None, label_color: Color = None, input_text_param: Param = None,
-             input_text_color: Color = None):
-        if label == None:
-            label = ""
-        if label_color == None:
-            label_color = Color.NULL
-        if input_text_color == None:
-            input_text_color = Color.NULL
-        if label_param == None:
-            label_param = Param.NULL
-        if input_text_param == None:
-            input_text_param = Param.NULL
+    def scan(label: str = "", label_param: Param = Param.NULL, label_color: Color = Color.NULL, input_text_param: Param = Param.NULL,
+             input_text_color: Color = Color.NULL) -> str:
+        """Read a string from stylized console input
+
+        :argument label: Text printed before input
+        :argument label_param: Label text parameter
+        :argument label_color: Label text color
+        :argument input_text_param: Input text parameter
+        :argument input_text_color: Input text color
+
+        :returns: Input string
+        """
 
         return str(input(Tool.OFF.value + str(label_param.value) + str(label_color.value) + str(label) + Tool.OFF.value + str(input_text_param.value) + str(input_text_color.value)))
